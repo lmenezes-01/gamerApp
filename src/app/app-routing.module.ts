0569c30d6b073ac63cae6bfe45fe5082db799d26
Gabriel <costa.gabriell@outlook.com>
2021-04-28 18:53:19 -0300

    Gabriel costa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'xbox',
+    loadChildren: () => import('./xbox/xbox.module').then( m => m.XboxPageModule)
   },
+
 ];
 
 @NgModule({
