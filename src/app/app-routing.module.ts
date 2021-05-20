44305b21dced3e32ef77f7b1c09099abf14bcd5a
Caio Mendes <caiommendesp@gmail.com>
2021-05-19 21:11:19 -0300

    Versão Final 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,33 +8,25 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'xbox',
     loadChildren: () => import('./xbox/xbox.module').then( m => m.XboxPageModule)
   },
-
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'playstation',
-    pathMatch: 'full'
-  },
   {
     path: 'playstation',
     loadChildren: () => import('./playstation/playstation.module').then( m => m.PlaystationPageModule)
   },
-
-];
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 
 ];
 
