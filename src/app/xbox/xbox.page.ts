8202c9a677b7f7e9b6b6e796b81253869a35c249
Gabriel <costa.gabriell@outlook.com>
2021-06-07 00:32:14 -0300

    Gabriel costa

--- src/app/xbox/xbox.page.ts
+++ src/app/xbox/xbox.page.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-xbox',
-  templateUrl: './xbox.page.html',
-  styleUrls: ['./xbox.page.scss'],
-})
-export class XboxPage implements OnInit {
-
-  constructor() { }
+import { Component, OnInit } from '@angular/core'; 
  
-  option = {
-    slidesPerView: 1.5,
-    centeredSlides: true,
-    loop: true,
-    spaceBetween: 10,
-    // autoplay: true,
-  }
-  ngOnInit(){
-    
-  }
+@Component({ 
+  selector: 'app-xbox', 
+  templateUrl: './xbox.page.html', 
+  styleUrls: ['./xbox.page.scss'],  
+}) 
+export class XboxPage implements OnInit { 
+ 
+  constructor() { } 
 
-
-}
+  option = { 
+   slidesPerView: 1.5,
+   centeredSlides: true,
+   loop: true,
+   spaceBetween: 10,
+   // autoplay: true,
+   }
+   ngOnInit(){
+     
+   }
+ 
+ 
+} 
