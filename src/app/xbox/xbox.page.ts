a9ff2ce6e72418fdd5255a83ac375bdcb7e6438e
Gabriel <costa.gabriell@outlook.com>
2021-06-06 23:48:56 -0300

    Gabriel costa

--- src/app/xbox/xbox.page.ts
+++ src/app/xbox/xbox.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class XboxPage implements OnInit {
 
   constructor() { }
-
+ //
   option = {
     slidesPerView: 1.5,
     centeredSlides: true,
