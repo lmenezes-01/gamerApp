60c9810249cd34c3cbf734101b75e67a73f09b54
Gabriel <costa.gabriell@outlook.com>
2021-06-08 17:22:15 -0300

    Gabriel costa

--- src/app/xbox/xbox.page.ts
+++ src/app/xbox/xbox.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
  
 @Component({ 
   selector: 'app-xbox', 
-  templateUrl: './xbox.page.html', 
+  templateUrl: './xbox.page.html',
   styleUrls: ['./xbox.page.scss'],  
 }) 
 export class XboxPage implements OnInit { 
